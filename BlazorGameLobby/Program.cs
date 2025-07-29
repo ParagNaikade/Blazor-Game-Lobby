6794d2c024e8ade089d7af3d50ebc28da6baa51f
Parag Naikade <parag.naikade@standards.org.au>
2025-07-29 15:39:03 +1000

    Add project files.

---
+++ BlazorGameLobby/Program.cs
@@ -0,0 +1,37 @@
+using BlazorGameLobby.Components;
+using BlazorGameLobby.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+app.MapBlazorHub();             // For Blazor Server real-time rendering
+app.MapHub<GameHub>("/gamehub"); // Your custom SignalR endpoint
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode();
+
+app.Run();
---
+++ GameLobby/Program.cs
@@ -0,0 +1,26 @@
+﻿using GameLobby.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+app.MapHub<GameHub>("/gamehub");
+//app.MapBlazorHub(); // For Blazor Server real-time rendering
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>() // Ensure 'App' is defined in the correct namespace
+    .AddInteractiveServerRenderMode();
+
+app.Run();
